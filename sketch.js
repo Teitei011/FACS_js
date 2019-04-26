2fe033e8a40b3a1cc3907a64283ee27ee9739c4e
Teitei011 <45126099+Teitei011@users.noreply.github.com>
2019-04-26 08:47:20 -0300

    Update where the checkResult happended. Emotino n definied

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 var black = (0,0,0);
 
 var repetitions = 10;
-var score;
 
-var answer;
+var score;
+var score_value;
+
+var temp;
+
+var guess;
 
 var Anger, Sadness, Fear, Surprise, Contempt, Disgust, Happyness;
-var emotion;
 var array_of_emotions = [];
 
 function setup(){
@@ -27,22 +30,26 @@ function setup(){
 }
 
 function createArrayOfEmotions(){
-  for (var i = 0;i < repetitions; ++i){
+  for (var i = 0; i < repetitions; ++i){
     array_of_emotions[i] = new Emotion();
   }
-
-  for (var i = 0;i < repetitions; ++i){
-    array_of_emotions[i].loadEmotionImage();
-  }
 }
-
 
 function draw() {
   background(black);
   // put drawing code here
 }
 
+function checkResult(guess){
+  temp = array_of_emotions.pop();
+  if (temp == guess) return true;
+  else return false;
+}
 
+function score_update(){
+  if (checkResult) ++score_value;
+  else --score_value;
+}
 
 function image_update(){
 
